4bfd45dd605e03ac078f11d83c7f440e7aae4a32
Kevin Berg <kevinmarkvi@gmail.com>
2018-05-23 19:48:11 -0700

    Add Framework

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Java-TestNG-Selenium-Gradle
+
+This code is provided on an "AS-IS” basis without warranty of any kind, either express or implied, including without limitation any implied warranties of condition, uninterrupted use, merchantability, fitness for a particular purpose, or non-infringement. Your tests and testing environments may require you to modify this framework. Issues regarding this framework should be submitted through GitHub. For questions regarding Sauce Labs integration, please see the Sauce Labs documentation at https://wiki.saucelabs.com/. This framework is not maintained by Sauce Labs Support.
+
+# Setup
+* Install Gradle 4.7
+
+# Variables
+* Set Sauce Labs credentials
+```
+$export SAUCE_USERNAME=<sauce username>
+$export SAUCE_ACCESS_KEY=<sauce access key>
+$export BUILD_TAG=<optional build id>
+```
+
+# Test Environment
+* Set desired capabilities [Sauce Labs Platform Configurator](https://wiki.saucelabs.com/display/DOCS/Platform+Configurator)
+```
+$export SELENIUM_BROWSER=<browser name>
+$export SELENIUM_VERSION=<browser version>
+$export SELENIUM_PLATFORM=<platform name>
+```
+
+# Run Tests
+* Run the gradle task as shown below
+```
+$./gradlew clean test
+```
+
+### Resources
+
+##### [Sauce Labs Documentation](https://wiki.saucelabs.com/)
+
+##### [TestNg Documentation](http://testng.org/javadocs/index.html)
+
+##### [Java Documentation](https://docs.oracle.com/javase/7/docs/api/)
+
+##### [SeleniumHQ Documentation](http://www.seleniumhq.org/docs/)
+
+##### [Gradle Documentation](http://gradle.org/documentation/)
+
+##### [Stack Overflow](http://stackoverflow.com/)
+* A great resource to search for issues not explicitly covered by documentation.
---
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -0,0 +1,110 @@
+package com.yourcompany.Tests;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.UnexpectedException;
+
+/**
+ * Simple TestNG test which demonstrates being instantiated via a DataProvider in order to supply multiple browser combinations.
+ *
+ * @author Neil Manvar
+ */
+public class TestBase  {
+
+    public String buildTag = System.getenv("BUILD_TAG");
+
+    public String username = System.getenv("SAUCE_USERNAME");
+
+    public String accesskey = System.getenv("SAUCE_ACCESS_KEY");
+
+    /**
+     * ThreadLocal variable which contains the  {@link WebDriver} instance which is used to perform browser interactions with.
+     */
+    private ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
+
+    /**
+     * ThreadLocal variable which contains the Sauce Job Id.
+     */
+    private ThreadLocal<String> sessionId = new ThreadLocal<String>();
+
+    /**
+     * @return the {@link WebDriver} for the current thread
+     */
+    public WebDriver getWebDriver() {
+        return webDriver.get();
+    }
+
+    /**
+     *
+     * @return the Sauce Job id for the current thread
+     */
+    public String getSessionId() {
+        return sessionId.get();
+    }
+
+    /**
+     * Constructs a new {@link RemoteWebDriver} instance which is configured to use the capabilities defined by the browser,
+     * version and os parameters, and which is configured to run against ondemand.saucelabs.com, using
+     * the username and access key populated by the {@link #authentication} instance.
+     *
+     * @param browser Represents the browser to be used as part of the test run.
+     * @param version Represents the version of the browser to be used as part of the test run.
+     * @param os Represents the operating system to be used as part of the test run.
+     * @param methodName Represents the name of the test case that will be used to identify the test on Sauce.
+     * @return
+     * @throws MalformedURLException if an error occurs parsing the url
+     */
+    protected void createDriver(String methodName)
+            throws MalformedURLException, UnexpectedException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        String browserName = System.getenv("SELENIUM_BROWSER");
+        String version = System.getenv("SELENIUM_VERSION");
+        String platform = System.getenv("SELENIUM_PLATFORM");
+
+        // set desired capabilities to launch appropriate browser on Sauce
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, browserName);
+        capabilities.setCapability(CapabilityType.VERSION, version);
+        capabilities.setCapability(CapabilityType.PLATFORM, platform);
+        capabilities.setCapability("name", methodName);
+
+        if (buildTag != null) {
+            capabilities.setCapability("build", buildTag);
+        }
+
+        // Launch remote browser and set it as the current thread
+        webDriver.set(new RemoteWebDriver(
+                new URL("https://" + username + ":" + accesskey + "@ondemand.saucelabs.com:443/wd/hub"),
+                capabilities));
+
+        // set current sessionId
+        String id = ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+        sessionId.set(id);
+    }
+
+    /**
+     * Method that gets invoked after test.
+     * Dumps browser log and
+     * Closes the browser
+     */
+    @AfterMethod
+    public void tearDown(ITestResult result) throws Exception {
+        ((JavascriptExecutor) webDriver.get()).executeScript("sauce:job-result=" + (result.isSuccess() ? "passed" : "failed"));
+        webDriver.get().quit();
+    }
+
+    protected void annotate(String text) {
+        ((JavascriptExecutor) webDriver.get()).executeScript("sauce:context=" + text);
+    }
+}
